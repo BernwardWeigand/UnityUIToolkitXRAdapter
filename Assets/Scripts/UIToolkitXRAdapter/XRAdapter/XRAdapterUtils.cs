7ddee2b811c7ad5621b88b631b6d920216896828
Bernward Weigand <bernward.weigand@posteo.de>
2021-02-22 12:37:18 +0100

    improve pinter debug

--- Assets/Scripts/UIToolkitXRAdapter/XRAdapter/XRAdapterUtils.cs
+++ Assets/Scripts/UIToolkitXRAdapter/XRAdapter/XRAdapterUtils.cs
@@ -45,17 +45,48 @@ namespace UIToolkitXRAdapter.XRAdapter {
                     .WithY(y => Screen.height + y));
 
         internal static void EnablePointerDebug(this UIDocument uiDocument) {
+            const int width = 15;
+            const int clickOffset = 1;
+
+            var pointerLeft = CreatePointerPart();
+            pointerLeft.style.height = new StyleLength(width + clickOffset * 3);
+            pointerLeft.style.width = new StyleLength(width);
+            uiDocument.rootVisualElement.Add(pointerLeft);
+
+            var pointerRight = CreatePointerPart();
+            pointerRight.style.height = new StyleLength(width + clickOffset * 2);
+            pointerRight.style.width = new StyleLength(width);
+            uiDocument.rootVisualElement.Add(pointerRight);
+
+
+            var pointerTop = CreatePointerPart();
+            pointerTop.style.height = new StyleLength(width);
+            pointerTop.style.width = new StyleLength(width + clickOffset * 3);
+            uiDocument.rootVisualElement.Add(pointerTop);
+
+
+            var pointerBottom = CreatePointerPart();
+            pointerBottom.style.height = new StyleLength(width);
+            pointerBottom.style.width = new StyleLength(width + clickOffset * 3);
+            uiDocument.rootVisualElement.Add(pointerBottom);
+
+            uiDocument.rootVisualElement.RegisterCallback<PointerMoveEvent>(evt => {
+                pointerLeft.transform.position = evt.localPosition.WithY(y => y - (width + clickOffset))
+                    .WithX(x => x - (width + clickOffset));
+                pointerBottom.transform.position = evt.localPosition.WithX(x => x - (width + clickOffset))
+                    .WithY(y => y + clickOffset);
+                pointerTop.transform.position = evt.localPosition.WithY(y => y - (width + clickOffset))
+                    .WithX(x => x - clickOffset);
+                pointerRight.transform.position = evt.localPosition.WithX(x => x + clickOffset)
+                    .WithY(y => y - clickOffset);
+            });
+        }
+
+        private static VisualElement CreatePointerPart() {
             var pointer = new VisualElement();
             pointer.style.backgroundColor = new StyleColor(Color.red);
             pointer.style.position = Position.Absolute;
-            const int size = 30;
-            pointer.style.height = new StyleLength(size);
-            pointer.style.width = new StyleLength(size);
-            uiDocument.rootVisualElement.Add(pointer);
-            const int clickOffset = 1;
-            uiDocument.rootVisualElement.RegisterCallback<PointerMoveEvent>(evt => {
-                pointer.transform.position = evt.localPosition.WithY(y => y + clickOffset).WithX(x => x + clickOffset);
-            });
+            return pointer;
         }
     }
 }
