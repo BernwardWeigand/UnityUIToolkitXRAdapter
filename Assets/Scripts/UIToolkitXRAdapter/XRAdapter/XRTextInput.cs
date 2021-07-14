fc81ff6fbad3a407a237a6950ae35ecf13209458
Bernward Weigand <bernward.weigand@anevis-solutions.com>
2021-07-14 16:51:22 +0200

    make inhertence more consistent

--- Assets/Scripts/UIToolkitXRAdapter/XRAdapter/XRTextInput.cs
+++ Assets/Scripts/UIToolkitXRAdapter/XRAdapter/XRTextInput.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+﻿using CoreLibrary;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace UIToolkitXRAdapter.XRAdapter {
     // TODO move to an interface as soon as a way is found to add interfaces in the editor
-    public abstract class XRTextInput: MonoBehaviour {
+    public abstract class XRTextInput: BaseBehaviour {
         public abstract void Activate(TextField textField);
         
         public abstract void Deactivate(TextField textField);
